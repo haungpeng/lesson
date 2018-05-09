11b0aac6e701a08fe63be99d0033f067087baa48
haungpeng <1337312919@qq.com>
2018-05-09 17:28:45 +0800

    提交

--- LS19/js/demo09.js
+++ LS19/js/demo09.js
@@ -15,18 +15,22 @@ console.log(Date.UTC(2017,9,1));//将给定的日期转换成毫秒,解释为UTC
 
 //Date原型方法 getter和setter相关
 var d = new Date("1978-11-25");
-console.log(d.getFullYear(),d.getMonth(),d.getDay(),d.getDate(),d.getHours());
-console.log(d.getTimezoneOffset());
-d.setDate(11);
-console.log(d.getFullYear(),d.getMonth(),d.getDay(),d.getDate(),d.getHours());
-d.setFullYear(1999,5,3);
-console.log(d.getFullYear(),d.getMonth(),d.getDay(),d.getDate(),d.getHours());
+console.log(d.getFullYear(),d.getMonth(),d.getDay(),d.getDate(),d.getHours());//1978 10 6 25 8
+console.log(d.getTimezoneOffset());//返回格林威治时间和本地时间之间的时差，以分钟为单位。
+d.setDate(11);//方法用于设置一个月的某一天。
+console.log(d.getFullYear(),d.getMonth(),d.getDay(),d.getDate(),d.getHours());//1978 10 6 11 8
+d.setFullYear(1999,5,3);//方法用于设置年份。
+console.log(d.getFullYear(),d.getMonth(),d.getDay(),d.getDate(),d.getHours());//1999 5 4 3 8
 
 //Date原型方法 转成字符串相关
 var d = new Date(2012,3,21,15,7,23,234);
-console.log(d.toTimeString(),"___",d.toLocaleTimeString());
-console.log(d.toDateString(),"___",d.toLocaleDateString());
+console.log(d.toTimeString(),"___",d.toLocaleTimeString());//15:07:23 GMT+0800 (中国标准时间) ___ 下午3:07:23
+console.log(d.toDateString(),"___",d.toLocaleDateString());//Sat Apr 21 2012 ___ 2012/4/21
 console.log(d.toJSON());
+//d.toTimeString():把 Date 对象的时间部分转换为字符串，并返回结果。
+//toLocaleTimeString():可根据本地时间把 Date 对象的时间部分转换为字符串，并返回结果。
+//toDateString():方法可把 Date 对象的日期部分转换为字符串，并返回结果。
+//toLocaleDateString():根据本地时间把 Date 对象的日期部分转换为字符串，并返回结果。
 
 // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Date
 
--- LS19/js/demo10.js
+++ LS19/js/demo10.js
@@ -10,17 +10,32 @@ console.log(new Date().toISOString());
 console.log(Date.parse("2010-01-01T11:12:23.111Z"));
 console.log(new Date("2010-01-01T11:12:23.111Z"));
 console.log(new Date().toISOString());
+//1262315543111
+//Fri Jan 01 2010 11:12:23 GMT+0800 (中国标准时间)
+//2018-05-09T08:55:32.176Z
+//1262344343111
+//Fri Jan 01 2010 19:12:23 GMT+0800 (中国标准时间)
+//2018-05-09T08:55:32.177Z
+
+//toISOString()可以使用ISO标准将 Date 对象转换为字符串。
 
 //日期格式（无时间）
 console.log(new Date("2001"));
 console.log(new Date("2001-02"));
 console.log(new Date("2001-02-22"));
+//Mon Jan 01 2001 08:00:00 GMT+0800 (中国标准时间)
+//Thu Feb 01 2001 08:00:00 GMT+0800 (中国标准时间)
+//Thu Feb 22 2001 08:00:00 GMT+0800 (中国标准时间)
 
 //日期和时间格式
 console.log(new Date("1999-11-22T13:17"));
 console.log(new Date("1999-11-22T13:17:11"));
 console.log(new Date("1999-11-22T13:17:11.111"));
 console.log(new Date("1999-11-22T13:17:11.111Z"));
+//Mon Nov 22 1999 13:17:00 GMT+0800 (中国标准时间)
+//Mon Nov 22 1999 13:17:11 GMT+0800 (中国标准时间)
+//Mon Nov 22 1999 13:17:11 GMT+0800 (中国标准时间)
+//Mon Nov 22 1999 21:17:11 GMT+0800 (中国标准时间)
 
 //时间的比较和运算，内部转换为数字进行比较和运算（从1970年1月1日00:00:00开始计算）
 console.log(new Date("1970-01-01").getTime());
@@ -29,9 +44,18 @@ console.log(new Date("1960-01-02").getTime());
 console.log(new Date("1970-01-02") > new Date("1970-01-01"));
 console.log(new Date("1970-01-02") - new Date("1970-01-01"));
 console.log(new Date((new Date("1970-01-01").getTime())+86400000));
+//0
+//86400000
+//-315532800000
+//true
+//86400000
+//Fri Jan 02 1970 08:00:00 GMT+0800 (中国标准时间)
 
 //时间运算 和 时间综合练习 输出50天后是几月几号，星期几？
-
+var time=Date.now();
+time+=50*24*60*60*1000;
+var time1=new Date(time);
+console.log(time1);
 
 
 
